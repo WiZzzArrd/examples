25db6b9cb6d42927373c6ab48e07d198e9ba9295
Edward <110904638+WiZzzArrd@users.noreply.github.com>
2023-06-14 13:23:35 +0500

    Add files via upload

---
+++ AppTestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Wizzard
+ * Date: 14.06.2023
+ * Time: 13:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AppTestProject
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
