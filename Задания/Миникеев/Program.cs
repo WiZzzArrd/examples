45bd1f48f8558ed32b5b821daaf43539eedff2fc
WiZzzArrd <eduardbordukov3@gmail.com>
2023-06-15 11:27:22 +0500

    +min

---
+++ Задания/Миникеев/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TaxiRent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance, price;
+
+            Console.WriteLine("Введите расстояние поездки в км:");
+            distance = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите регион поездки: 1 - в черте города, 2 - в радиусе до 70 км, 3 - в радиусе более 70 км");
+            int region = Int32.Parse(Console.ReadLine());
+
+            if (region == 1)
+            {
+                price = distance * 20;
+            }
+            else if (region == 2)
+            {
+                price = distance * 25;
+            }
+            else if (region == 3)
+            {
+                price = distance * 30;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный регион");
+                return;
+            }
+
+            Console.WriteLine("Цена поездки: " + price + " руб.");
+        }
+    }
+}
